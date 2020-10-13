cc214cc60daab0da5d16be9e663b75c7ce9da0cd
vsk4 <saikumarvadla4@gmail.com>
2020-10-13 09:48:20 +0530

    [VSK]: Added UC1_CreateGameBoard

---
+++ TicTacToe/TicTacToe/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            char[] board = tictactoeBoard();
+        }
+
+        private static char[] tictactoeBoard()
+        {
+            char[] board = new char[10];
+            for (int i =0; i< board.Length; i++)
+            {
+                board[i] = ' ';
+            }
+            return board;
+        }
+    }
+}
